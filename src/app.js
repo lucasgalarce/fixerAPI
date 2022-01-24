34b41b6bd0356575e246e520da019ac2ba4145be
lucasgalarce <galarcelucas@gmail.com>
2022-01-24 19:16:29 -0300

    createrates endpoint

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import Hapi from '@hapi/hapi';
 import fetch from 'node-fetch';
 import './database.js';
+import { v4 as uuidv4 } from 'uuid';
+import Rates from './models/rates.js';
+
 
 const init = async () => {
 
@@ -14,13 +17,38 @@ const init = async () => {
     
     server.route({
         method: 'GET',
-        path: '/',
+        path: '/createrates',
         handler: async (request, h) => {
 
-            const response  = await fetch(`http://data.fixer.io/api/latest?access_key=824e753b9d8f1bf170e5adf80e7788e9&symbols=USD,ARS,BRL`);
-            const body = await response.text();
-            console.log(body)
-            return 'Hello World!';
+            try {
+
+                const response  = await fetch(`http://data.fixer.io/api/latest?access_key=824e753b9d8f1bf170e5adf80e7788e9&symbols=USD,ARS,BRL`);
+                const body = await response.text();
+                const data = JSON.parse(body);
+
+                const rateId = uuidv4();
+    
+                const newRates = new Rates({
+                    id: rateId,
+                    timestamp: data.timestamp,
+                    base: data.base,
+                    date: data.date,
+                    originalRates: data.rates,
+                    fee: 5,
+                });
+        
+                const saveRates = await newRates.save();
+                console.log(saveRates)
+
+                // res.status(200).json({
+                //     Response: true,
+                //     Message: 'Rates created.',
+                // });
+                return 'ok'
+            } catch(error) {
+                console.log(error)
+            }
+
         }
     });
 };
